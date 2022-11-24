e62347777dc62ab92821334fe9dd240d74b2485b
ddt-kobayashi <ddt.rys.kobayashi@gmail.com>
2022-11-24 23:02:07 +0900

    お疲れ様です。 本日は演習問題の修正を行いました。 ご確認をお願い致します。

--- kensyuu1/src/kensyuu4/ensyuu4_20.java
+++ kensyuu1/src/kensyuu4/ensyuu4_20.java
@@ -22,18 +22,14 @@ public class ensyuu4_20 {
 		//入力値の値がinitial以上であるまで、改行し続ける（条件式）
 		//initialは処理を行うたびにインクリメントする（変更式）
 		for (int initial = 1; initial <= valueN; initial++) {
+			System.out.println();
 			//このfor文での処理が1個目のループ処理
 			//int型の変数hangの値はスタート地点の整数値を意味して1として定義する（初期値）
-			//入力値の値がhang以上である場合、＊を出力し続ける（条件式）
+			//入力値の値がhang以上である場合、＊と空白を出力し続ける（条件式）
 			//hangは処理を行うたびにインクリメントする（変更式）
 			for (int hang = 1; hang <= valueN; hang++) {
 				System.out.print('*');
-				//もしhangの値がvalueN以上の値の場合は改行、そうでなければ空白を出力する
-				if (hang >= valueN) {
-					System.out.println();
-				} else {
-					System.out.print(" ");
-				}
+				System.out.print(" ");	//空白は回答結果参照
 			}
 		}
 	}
