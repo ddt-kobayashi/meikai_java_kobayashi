0419a204de6c1044d5750718565181ad532c1dd7
ddt-kobayashi <ddt.rys.kobayashi@gmail.com>
2022-12-07 01:00:54 +0900

    夜分遅くに失礼します。 お疲れ様です。 本日は、演習問題6−3を行いました。 ご確認をお願い致します。

---
+++ kensyuu1/src/kensyuu6/ensyuu6_3.java
@@ -0,0 +1,22 @@
+//要素型がdouble型で要素数が5の配列の要素に対して、先頭から順に1.1、2.2、3.3、4.4、5.5を代入して表示するプログラム
+
+package kensyuu6;
+
+public class ensyuu6_3 {
+	public static void main(String[] args) {
+		//構成要素型がdouble型で構成要素数は５
+		double[] array = new double[5];
+
+		//array.lengthにて配列の要素数（長さ）を取得
+		for (int algebra = 0; algebra < array.length; algebra++) {
+			int standard = algebra + 1;
+			//配列の中の元となる値を生成
+			double arrayValue = (standard * 11);
+			//先頭から順に1.1、2.2、3.3、4.4、5.5を代入
+			//int型からdouble型へのキャスト演算子を利用
+			array[algebra] = (double) arrayValue / 10;
+			//全要素の値を表示
+			System.out.println(array[algebra]);
+		}
+	}
+}
